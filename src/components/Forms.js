78b9366ce2d62147d408eaae30769fcae850c197
Samrat1507 <lazyworm3325@gmail.com>
2022-11-05 12:55:44 +0530

    updated

--- src/components/Forms.js
+++ src/components/Forms.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 const Forms = () => {
   return (
-    <div className='flex flex-col md:flex-row absolute text-center w-full bg-slate-300 justify-evenly '>
-      <div className=''>
-        Sign Up
+    <body class="h-screen bg-gradient-to-b from-blue-500 to-blue-200">
+    <div className='flex flex-col md:flex-row absolute text-center w-full content-between bg-gray-300 justify-evenly items-center rounded-3xl border-black top-[25%]'>
+
+      <div className='form frm1'>
+        <p className='label lbl1'>Sign Up</p>
+        
         <form className='flex flex-col space-y-2 mt-4 md:mt-0 w-fit mx-auto'>
           <div className='flex space-x-2'>
           <input className='contactInput' placeholder='First Name'/>
@@ -14,16 +17,31 @@ const Forms = () => {
           <input className='contactInput' type="text" placeholder='Password'/>
           <input className='contactInput' type="text" placeholder='Roll No./Faculty ID'/>
           <input className='contactInput' type="text" placeholder='School affiliated to e.g CSE/IT..'/>
+          <input className='contactInput' type="text" placeholder='Designation (optional)'/>
+          
+          <button class="bg-gray-200 hover:bg-rose-300 text-black font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-full">
+  Submit
+</button>
+
+            
+        
         </form>
       </div>
-      <div className=''>
-        Login
+    
+      <div className='form frm2'>
+        <p className='label lbl2'>Login</p>
+        
         <form className='flex flex-col space-y-2 mt-4 md:mt-0 w-fit mx-auto'>
           <input className='contactInput' type="text" placeholder='Email'/>
           <input className='contactInput' type="text" placeholder='Password'/>
+          <button class="bg-gray-200 hover:bg-rose-300 text-black font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded-full">
+
+  Apply
+</button>
         </form>
       </div>
     </div>
+    </body>
   )
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import kevents from "../kevents.png"
 
 const Navbar = () => {
   return (
-    <nav className='flex flex-col md:flex-row justify-between'>
-        <div>KIIT LOGO</div>
-        <div>KEVENTS</div>
-        <div>DEVSPRINT</div>
+      
+    <nav className='flex flex-col md:flex-row justify-between h-20 bg-gradient-to-b from-zinc-500 to-zinc-900'>
+        
+        <div>
+          <img className='h-20' src="https://upload.wikimedia.org/wikipedia/en/thumb/e/ef/KIIT_logo.svg/1200px-KIIT_logo.svg.png" alt="Kiit"></img>
+      
+          </div>
+        
+        
+        <div> 
+        <img class="h-20 " src= {kevents} alt="kiitlogo">
+          </img>
+        </div>
+        
+        
+        <div>
+        <img class="h-12 w-15 " src="https://devsprint.dsckiit.in/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo.37eabc0a.png&w=3840&q=75" alt="Devsprint"></img>
+        </div>
+        
     </nav>
   )
 }
